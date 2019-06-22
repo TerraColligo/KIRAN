e1b95bbc21fc3fa5e819a537f199d5c0eb63ccb0
Kiran Kantesariya <kirankantesariya@gmail.com>
2019-06-22 10:56:46 +0530

    [Add] Sales Commission,data Export. Restriction

---
+++ tc_contacts_data_export/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Contact Export',
+    'version': '12.0.21.06.2019',
+    'category': 'base',
+    'depends': ['contacts', 'account', 'sun_sales_commission'],
+    'author': "Kiran Kantesariya",
+    'license': 'OPL-1',
+    'website': '',
+    'data': [
+        'wizard/export_partner_view.xml',
+    ],
+    "qweb": [],
+    'installable': True,
+    'application': True,
+ }
